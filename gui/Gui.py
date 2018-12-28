74970ec760ac00950f93cef31041bcb261b7bc9a
16028414 <44360462+16028414@users.noreply.github.com>
2018-12-28 15:22:24 +0000

    Added dadabes conectiomn
    added database conection and code for creating new entry into dispanlry action.

--- gui/Gui.py
+++ gui/Gui.py
@@ -19,6 +19,27 @@ y = (screen_height/2) - (height/2)
 root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 root.resizable(0, 0)
 
+def Database():
+    global conn, cursor
+    conn = sqlite3.connect('UfixLtd.s3db')
+    cursor = conn.cursor()
+    cursor.execute("CREAT TABE IF NOT EXSITS 'Disciplinary_Action' (emp id INTEGER PRIMARY KEY AUTOINCMENT NOT NULL, Discipline_ID INTEGER, Employee_ID INTEGER, Comment TEXT")
+    #connects the displanryaction table
+
+def AddNewRecord():
+    if  DISCIPLINARY_ID.get() == "" or EMPLOYEE_ID.get() == "" or COMMENT.get() == "":  #contius for all data to be enterd
+        #what are the .get() for? the table feilds or textboxs?
+        txt.resulrs.congif(text= "pleasa complete the reierd feld", fg="red")
+    else:#creats new record
+        Database()
+        cursor.execute("INSERT INTO 'Disciplinary_list' (Discipline_ID, Employee_ID, Comment) VALUES{?, ?, ?}", str(S))
+        conn.commit()
+        DISCIPLINARY_ID.set("")#will this work for combo boxes
+        EMPLOYEE_ID.set("")
+        COMMENT.set("")
+        cursor.close()
+        conn.close()
+        txt.resulrs.congif(text="New recored created", fg="green")
 
 def exit_program():  # asks the user if they want to exit
     result = tkMessageBox.askquestion('HR Demo Module', 'Are you sure you want to exit?', icon="warning")
