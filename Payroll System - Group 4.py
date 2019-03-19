1add6767df25f2b42822e48c824903df332f6163
EnsaTouray <44966739+EnsaTouray@users.noreply.github.com>
2019-03-19 17:08:33 +0000

    Add files via upload

--- Payroll System - Group 4.py
+++ Payroll System - Group 4.py
@@ -10,21 +10,13 @@ def mainMenu():
     closeprev()
     global screen
     screen = Tk()
-    screen.geometry("350x400")
+    screen.geometry("350x200")
     screen.title("Menu")
     Label(screen, text="Staff register", bg="black", fg="white", width=500, height = 2, font=("calibri", 18)).pack()
     Label(screen).pack()
     Label(screen).pack()
-    Label(screen).pack()
-    Button(screen, text="Sign in", borderwidth=2, relief="solid",  height=3, width="35", bg="white", fg="black", font=("calibri", 12), command=loginForm).pack()
-    Label(screen).pack()
-    Button(screen, text="Sign out", borderwidth=2, relief="solid", height="3", width="35", bg="white", fg="black", font=("calibri", 12), command=logoutForm).pack()
-    Label(screen).pack()
-    Label(screen).pack()
-    Label(screen).pack()
     Button(screen, text="Admin", borderwidth=2, relief="solid", height="1", width="35", bg="white", fg="black",
            font=("calibri", 12), command=AdminLoginForm).pack()
-
     screen.mainloop()
 
 def AdminForm():
@@ -67,68 +59,9 @@ def AdminLoginForm():
     Button(screen4, text="cancel", borderwidth=2, relief="solid", height=1, width="35", bg="white", fg="black",
            font=("calibri", 12), command=mainMenu).pack()
 
-def loginForm():
-    global prev
-    prev=1
-    screen.destroy()
-    global  screen1
-    screen1 = Tk()
-    screen1.title("Sign in")
-    screen1.geometry("350x400")
-
-    username=StringVar()
-    password=StringVar()
-
-    Label(screen1, text="Login", bg="white", fg="black", width=500, height="2", font=("calibri", 18)).pack()
-    Label(screen1).pack()
-    Label(screen1).pack()
-    Label(screen1, text = "Employee ID", bg="black", fg="white", width="300", height=1, font=("calibri", 12)).pack()
-    Entry(screen1, textvariable = username, width="300", bg="white", fg="black", font=("calibri", 16)).pack()
-    Label(screen1).pack()
-    Label(screen1, text="Password", bg="black", fg="white", width=100, height=1,  font=("calibri", 12,)).pack()
-    Entry(screen1, textvariable=password,  width=150, bg="white", fg="black",  font=("calibri", 16)).pack()
-    Label(screen1).pack()
-    Label(screen1).pack()
-    Button(screen1, text="log in", borderwidth=2, relief="solid", height=1, width="35", bg="white", fg="black",
-           font=("calibri", 12), command=login).pack()
-    Label(screen1).pack()
-    Button(screen1, text="cancel", borderwidth=2, relief="solid", height=1, width="35", bg="white", fg="black",
-           font=("calibri", 12), command=mainMenu).pack()
-
-def logoutForm():
-    global prev
-    prev = 2
-    global screen2
-    screen.destroy()
-    screen2 = Tk()
-    screen2.title("Sign out")
-    screen2.geometry("350x400")
-
-    username = StringVar()
-    password = StringVar()
-
-    Label(screen2, text="Log out", bg="white", fg="black", width=500, height="2", font=("calibri", 18)).pack()
-    Label(screen2).pack()
-    Label(screen2).pack()
-    Label(screen2, text="Employee ID", bg="black", fg="white", width="300", height=1, font=("calibri", 12)).pack()
-    Entry(screen2, textvariable=username, width="300", bg="white", fg="black", font=("calibri", 16)).pack()
-    Label(screen2).pack()
-    Label(screen2, text="Password", bg="black", fg="white", width=100, height=1, font=("calibri", 12,)).pack()
-    Entry(screen2, textvariable=password, width=100, bg="white", fg="black", font=("calibri", 16)).pack()
-    Label(screen2).pack()
-    Label(screen2).pack()
-    Button(screen2, text="log out", borderwidth=2, relief="solid", height=1, width="35", bg="white", fg="black",
-           font=("calibri", 12), command=logout).pack()
-    Label(screen2).pack()
-    Button(screen2, text="cancel", borderwidth=2, relief="solid", height=1, width="35", bg="white", fg="black",
-           font=("calibri", 12), command=mainMenu).pack()
 
 def closeprev():
-    if prev == 1:
-        screen1.destroy()
-    elif prev == 2:
-        screen2.destroy()
-    elif prev == 3:
+    if prev == 3:
         screen3.destroy()
     elif prev == 4:
         screen4.destroy()
@@ -148,11 +81,12 @@ def checkcredentials():
         find_user = ("SELECT * FROM UFIX_PIM WHERE username =? AND password = ?")
         cursor.execute(find_user,[(username_input), (password_input)])
         results = cursor.fetchall()
-        print (results)
+        #print (results)
 
         if results:
             for i in results:
                 print ("welcome " + i[2])
+                AdminForm()
             return ("exit")
 
         else:
