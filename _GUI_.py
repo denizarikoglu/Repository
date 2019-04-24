ff2a5b3d99930b664381cb08fde7185ac2c28c74
CallumBowden <43952538+CallumBowden@users.noreply.github.com>
2019-04-24 13:36:33 +0100

    GUI_ReadyForIntegration
    the ability to change pages is there now, for integration of other teams code

--- _GUI_.py
+++ _GUI_.py
@@ -3,18 +3,7 @@ import sqlite3
 import tkinter.ttk as ttk
 import tkinter.messagebox as tkMessageBox
 #import io.StringIO
-
-
-root = Tk()
-root.title("Login Page")
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-width = 300
-height = 500
-x = (screen_width/2) - (width/2)
-y = (screen_height/2) - (height/2)
-root.geometry('%dx%d+%d+%d' % (width, height, x, y))
-root.resizable(0, 0)
+import tkinter as tk
 
 ############Methods############
 
@@ -32,38 +21,88 @@ def LegalInfo():
     tkMessageBox.showinfo('Legal Notice', 'Reminder - Coding practises and ethical standards are to be met at all times')
 
 ############Frame############
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
 
-#Top = Frame(root, width = 900, height = 50, bd = 8, relief = "raise")
-#Top.pack(side = TOP)
+class Page_Home(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is the HOME page, navigate from here")
+       label.pack(side="top", fill="both", expand=True)
 
-Left = Frame(root, width = 300, height = 500, bd = 8, relief = "raise")
-Left.pack(side = LEFT)
+class Page_Departments(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = tk.Label(self, text="This will bring up DEPARTMENT navigation page")
+        label.pack(side="top", fill="both", expand=True)
 
-#Right = Frame(root, width = 600, height = 500, bd = 8, relief = "raise")
-#Right.pack(side = RIGHT)
+class Page_Support(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This will bring up SUPPORT page")
+       label.pack(side="top", fill="both", expand=True)
 
-Forms = Frame(Left, width = 300, height = 500)
-Forms.pack(side = TOP)
+class Page_Services(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This will bring up SERVICES page")
+       label.pack(side="top", fill="both", expand=True)
 
-#Buttons = Frame(Left, width = 100, height = 100, bd = 8, relief = "raise")
-#Buttons.pack(side = BOTTOM)
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page_Home(self)
+        p2 = Page_Departments(self)
+        p3 = Page_Support(self)
+        p4 = Page_Services(self)
 
-############Buttons############
-#exit button
-btn_exit = Button(root, width = 10, text = 'EXIT', command = root.quit)
-btn_exit.place(x = 20, y = 450)
-#support button
-button_1 = Button(root, text = "Support")
-button_1.place(x = 20, y = 50)
-#services button
-button_2 = Button(root, text = "Services")
-button_2.place(x = 20, y = 150)
-#department button
-button_3 = Button(root, text = "Department")
-button_3.place(x = 20, y = 250)
-#legal doc button
-button_4 = Button(root, text = "Legal Documentation", command = LegalInfo)
-button_4.place(x = 20, y = 350)
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
 
-if __name__ == '__main__':
+        # p1 = home
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        # p2 = departments
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        # p3 = support
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        # p4 = services
+        p4.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        ############Buttons############
+        # Home Page
+        # exit button
+        btn_exit = Button(root, width=10, text='EXIT', command=root.quit)
+        btn_exit.place(x=20, y=450)
+        # support button
+        button_1 = Button(root, text="Support", command=p3.lift)
+        button_1.place(x=20, y=50)
+        # services button
+        button_2 = Button(root, text="Services", command=p4.lift)
+        button_2.place(x=20, y=150)
+        # department button
+        button_3 = Button(root, text="Department", command=p2.lift)
+        button_3.place(x=20, y=250)
+        # legal doc button
+        button_4 = Button(root, text="Legal Documentation", command=LegalInfo)
+        button_4.place(x=20, y=350)
+
+        # Department Page - for integration
+
+        # Support Page - for integration
+
+        # Services Page - for integration
+
+        p1.show()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("UFIX LTD.")
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("480x500")
     root.mainloop()
